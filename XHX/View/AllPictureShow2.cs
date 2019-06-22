f34b99815ddca41c85bc2dbbd53c1d5e627e7526
Tony <moujunsheng15@163.com>
2019-06-22 23:09:37 +0800

    修改了批量查看图片

--- XHX/View/AllPictureShow2.cs
+++ XHX/View/AllPictureShow2.cs
@@ -15,6 +15,13 @@ namespace XHX.View
     public partial class AllPictureShow2 : DevExpress.XtraEditors.XtraForm
     {
         localhost.Service service = new XHX.localhost.Service();
+        string _filePath = "";
+        string[] _fileName = null; 
+        string _shopName = "";
+        string _subjectCode="";
+        string _type="";
+        string _code = "";
+        int _maxLen = 1;
 
         public AllPictureShow2()
         {
@@ -36,61 +43,99 @@ namespace XHX.View
         /// <param name="subjectCode"></param>
         /// <param name="type"></param>
         /// <param name="code"></param>
-        public AllPictureShow2(string filePath, string[] fileName, string shopName, string subjectCode,string type, string code)
+        public AllPictureShow2(string filePath, string[] fileName, string shopName, string subjectCode, string type, string code)
             : this()
         {
             this.LookAndFeel.SetSkinStyle(CommonHandler.Skin_Name);
+             _filePath = filePath;
+             _fileName = fileName;
+             _shopName = shopName;
+             _subjectCode = subjectCode;
+             _type = type;
+             _code = code;
+             _maxLen = fileName.Length;
+             lblCurrent.Text = "1";
+             lblSum.Text = fileName.Length.ToString();// CalEndIndex(1).ToString();
+             PictureShowFive(filePath, fileName, shopName, subjectCode, type, code, 1);
+
+        }
+        public void PictureShowFive(string filePath, string[] fileName, string shopName, string subjectCode, string type, string code, int startIndex)
+        {
             List<Image> pictures = new List<Image>();
 
-            for (int i = 0; i < fileName.Length; i++)
-            {
+            //for (int i = startIndex - 1; i < endIndex; i++)
+            //{
                 byte[] bytes = null;
                 if (type == "SpecialCase" || type == "Notice")
                 {
-                    bytes = service.SearchAnswerDtl2Pic(fileName[i], shopName, subjectCode, type, code);
+                    bytes = service.SearchAnswerDtl2Pic(fileName[startIndex-1], shopName, subjectCode, type, code);
                 }
                 else
                 {
-                    bytes = service.SearchAnswerDtl2Pic(fileName[i].Replace(".jpg", ""), shopName, subjectCode, type, code);
+                    bytes = service.SearchAnswerDtl2Pic(fileName[startIndex-1].Replace(".jpg", ""), shopName, subjectCode, type, code);
                 }
                 if (bytes != null && bytes.Length != 0)
                 {
                     MemoryStream ms = new MemoryStream(bytes);
                     Image image = Image.FromStream(ms);
                     pictures.Add(image);
+                    lblWarn.Text = "";
                 }
-            }
+                else {
+                    lblWarn.Text = "无照片";
+                }
+            //}
 
             if (pictures.Count != 0)
             {
                 this.kpImageViewer1.ImageList = pictures;
+
             }
             else
             {
-                for (int i = 0; i < fileName.Length; i++)
-                {
-                    if (type != "SpecialCase")
-                    {
-                        if (File.Exists(filePath + fileName[i] + ".jpg"))
-                        {
-                            Image image = Image.FromFile(filePath + fileName[i] + ".jpg");
-                            pictures.Add(image);
-                        }
-                    }
-                    else
-                    {
-                        if (File.Exists(filePath + fileName[i]))
-                        {
+                //for (int i = 0; i < fileName.Length; i++)
+                //{
+                //    if (type != "SpecialCase")
+                //    {
+                //        if (File.Exists(filePath + fileName[i] + ".jpg"))
+                //        {
+                //            Image image = Image.FromFile(filePath + fileName[i] + ".jpg");
+                //            pictures.Add(image);
+                //        }
+                //    }
+                //    else
+                //    {
+                //        if (File.Exists(filePath + fileName[i]))
+                //        {
 
-                            Image image = Image.FromFile(filePath + fileName[i]);
-                            pictures.Add(image);
-                        }
-                    }
-                }
-                this.kpImageViewer1.ImageList = pictures;
+                //            Image image = Image.FromFile(filePath + fileName[i]);
+                //            pictures.Add(image);
+                //        }
+                //    }
+                //}
+                this.kpImageViewer1.Image = Properties.Resources.imgWarning;
             }
         }
-       
-        
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(lblCurrent.Text) == 1)
+            {
+                return;
+            }
+            PictureShowFive(_filePath, _fileName, _shopName, _subjectCode, _type, _code, Convert.ToInt32(lblCurrent.Text) - 1);
+            lblCurrent.Text = (Convert.ToInt32(lblCurrent.Text) -1).ToString();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(lblCurrent.Text) == _fileName.Length)
+            {
+                return;
+            }
+            PictureShowFive(_filePath, _fileName, _shopName, _subjectCode, _type, _code, Convert.ToInt32(lblCurrent.Text) + 1);
+            lblCurrent.Text = (Convert.ToInt32(lblCurrent.Text) + 1).ToString();
+        }
+
     }
 }
--- XHX/View/AllPictureShow2.designer.cs
+++ XHX/View/AllPictureShow2.designer.cs
@@ -28,8 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
             this.kpImageViewer1 = new KaiwaProjects.KpImageViewer();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWarn = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblWarn);
+            this.panel1.Controls.Add(this.lblSum);
+            this.panel1.Controls.Add(this.lblCurrent);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnPre);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 42);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(367, 11);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "下一张";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(223, 11);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(75, 23);
+            this.btnPre.TabIndex = 5;
+            this.btnPre.Text = "上一张";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
             // 
             // kpImageViewer1
             // 
@@ -37,10 +79,11 @@
             this.kpImageViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.kpImageViewer1.GifAnimation = false;
             this.kpImageViewer1.Image = null;
-            this.kpImageViewer1.Location = new System.Drawing.Point(0, 0);
+            this.kpImageViewer1.ImageList = null;
+            this.kpImageViewer1.Location = new System.Drawing.Point(0, 42);
             this.kpImageViewer1.MenuColor = System.Drawing.Color.LightSteelBlue;
             this.kpImageViewer1.MenuPanelColor = System.Drawing.Color.LightSteelBlue;
-            this.kpImageViewer1.MinimumSize = new System.Drawing.Size(454, 145);
+            this.kpImageViewer1.MinimumSize = new System.Drawing.Size(618, 197);
             this.kpImageViewer1.Name = "kpImageViewer1";
             this.kpImageViewer1.NavigationPanelColor = System.Drawing.Color.LightSteelBlue;
             this.kpImageViewer1.NavigationTextColor = System.Drawing.SystemColors.ButtonHighlight;
@@ -51,25 +94,71 @@
             this.kpImageViewer1.PreviewTextColor = System.Drawing.SystemColors.ButtonHighlight;
             this.kpImageViewer1.Rotation = 0;
             this.kpImageViewer1.ShowPreview = true;
-            this.kpImageViewer1.Size = new System.Drawing.Size(826, 550);
-            this.kpImageViewer1.TabIndex = 0;
+            this.kpImageViewer1.Size = new System.Drawing.Size(964, 600);
+            this.kpImageViewer1.TabIndex = 4;
             this.kpImageViewer1.TextColor = System.Drawing.SystemColors.ButtonHighlight;
             // 
-            // Form1
+            // lblSum
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(342, 15);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(14, 14);
+            this.lblSum.TabIndex = 11;
+            this.lblSum.Text = "1";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(309, 15);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(14, 14);
+            this.lblCurrent.TabIndex = 10;
+            this.lblCurrent.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 14);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "/";
+            // 
+            // lblWarn
+            // 
+            this.lblWarn.AutoSize = true;
+            this.lblWarn.ForeColor = System.Drawing.Color.Red;
+            this.lblWarn.Location = new System.Drawing.Point(458, 15);
+            this.lblWarn.Name = "lblWarn";
+            this.lblWarn.Size = new System.Drawing.Size(0, 14);
+            this.lblWarn.TabIndex = 12;
+            // 
+            // AllPictureShow2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 550);
+            this.ClientSize = new System.Drawing.Size(964, 642);
             this.Controls.Add(this.kpImageViewer1);
-            this.Name = "Form1";
+            this.Controls.Add(this.panel1);
+            this.Name = "AllPictureShow2";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private KaiwaProjects.KpImageViewer kpImageViewer1;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWarn;
     }
 }
 
